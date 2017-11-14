774236649b73d01cb45c362db2e83952d7306eeb
Romuald Nave <github@committers.org>
2017-11-14 18:01:27 +0100

    Add contentful-import and importToSpace functions.

--- packages/wordpress-exporter-cli/src/wordpress-exporter/contentful.js
+++ packages/wordpress-exporter-cli/src/wordpress-exporter/contentful.js
@@ -1,7 +1,16 @@
 import { createClient } from 'contentful-management';
+import spaceImport from 'contentful-import';
 
 export const client = createClient({
   accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
 });
+
+export function importToSpace(spaceId, { entries = [], contentTypes = [], assets = [] } = {}) {
+  spaceImport({
+    content: { entries, contentTypes, assets },
+    spaceId,
+    managementToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+  });
+}
 
 export default client;
